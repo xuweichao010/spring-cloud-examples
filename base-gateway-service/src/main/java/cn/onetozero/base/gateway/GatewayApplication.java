d65385f5bd07414e89b9fb793439405be224556d
xuweichao <xuweichao010@163.com>
2023-03-23 13:40:22 +0800

    feat 提交代码

---
+++ base-gateway-service/src/main/java/cn/onetozero/base/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package cn.onetozero.base.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 类描述：
+ * 作者：徐卫超 (cc)
+ * 时间 2023/3/22 17:48
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
